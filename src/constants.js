3b2092bb59e32a3fb9e1bc6b0dbf8f0c2488910b
Jayash Saini <jayashysaini7361@gmail.com>
2024-07-02 23:39:07 +0530

    feat(user): implement user model

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,22 @@
 const DB_NAME = 'webworldcluster';
 
-module.exports = { DB_NAME };
+const UserRolesEnum = {
+  ADMIN: 'ADMIN',
+  USER: 'USER',
+};
+const AvailableUserRoles = Object.values(UserRolesEnum);
+
+const UserLoginType = {
+  EMAIL_PASSWORD: 'EMAIL_PASSWORD',
+  GOOGLE: 'GOOGLE',
+};
+
+const AvailableSocialLogins = Object.values(UserLoginType);
+
+module.exports = {
+  DB_NAME,
+  AvailableUserRoles,
+  UserRolesEnum,
+  UserLoginType,
+  AvailableSocialLogins,
+};
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,32 +1,61 @@
-import mongoose, { Schema } from 'mongoose';
+const mongoose = require('mongoose');
+const {
+  AvailableSocialLogins,
+  AvailableUserRoles,
+  UserRolesEnum,
+  UserLoginType,
+} = require('../constants');
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    avatar: {
-        type: String,
-        required: true
-    },
-    loginType: {
-        type: String,
-        required: true,
-        enum: ['local', 'google'],
-        default: 'local'
-    }
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    index: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true,
+  },
+  role: {
+    type: String,
+    enum: AvailableUserRoles,
+    default: UserRolesEnum.USER,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  loginType: {
+    type: String,
+    enum: AvailableSocialLogins,
+    default: UserLoginType.EMAIL_PASSWORD,
+  },
+  isEmailVerified: {
+    type: Boolean,
+    default: false,
+  },
+  refreshToken: {
+    type: String,
+  },
+  forgotPasswordToken: {
+    type: String,
+  },
+  forgotPasswordExpiry: {
+    type: Date,
+  },
+  emailVerificationToken: {
+    type: String,
+  },
+  emailVerificationExpiry: {
+    type: Date,
+  },
 });
 
-const User = mongoose.model("User", userSchema);
-
-export default User;
+const User = mongoose.model('User', userSchema);
+module.export = User;
